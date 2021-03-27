27d7981c9610fbfec76484627d44c24c3a50c49f
r3project <piotr.redek@gmail.com>
2021-03-27 17:04:37 +0100

    Updates

---
+++ precache-manifest.861e702c9709c86e088fa64e21dfc790.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f96bfe9b58205fb8a65235b81edb6613",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6a29fe2e992786c426d6",
+    "url": "./static/css/main.87f99b5d.chunk.css"
+  },
+  {
+    "revision": "34f2c880904427332122",
+    "url": "./static/js/2.b11d8b08.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.b11d8b08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a29fe2e992786c426d6",
+    "url": "./static/js/main.f1edace3.chunk.js"
+  },
+  {
+    "revision": "226c4e9fe1af34759647",
+    "url": "./static/js/runtime-main.1c74911c.js"
+  }
+]);
